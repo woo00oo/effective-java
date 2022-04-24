d17606213bf23cdf8e91701a374a236ec9919a19
woo00oo <96woo94@naver.com>
2022-04-24 21:49:19 +0900

    이펙티브 자바 완벽 공략 : 아이템 1. 완벽 공략5 - 리플렉션

--- src/main/java/com/example/effectivejava/chapter01/item01/HelloServiceFactory.java
+++ src/main/java/com/example/effectivejava/chapter01/item01/HelloServiceFactory.java
@@ -1,16 +1,24 @@
 package com.example.effectivejava.chapter01.item01;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Optional;
 import java.util.ServiceLoader;
 
 public class HelloServiceFactory {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         ServiceLoader<HelloService> loader = ServiceLoader.load(HelloService.class);
         Optional<HelloService> helloServiceOptional = loader.findFirst();
         helloServiceOptional.ifPresent(h -> {
             System.out.println(h.hello());
         });
+
+        Class<?> aClass = Class.forName("com.example.effectivejava.chapter01.item01.KoreanHelloService");
+        Constructor<?> constructor = aClass.getConstructor();
+        HelloService helloService = (HelloService) constructor.newInstance();
+        helloService.hello();
+
     }
 
 }
