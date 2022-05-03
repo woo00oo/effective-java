2a92528ab9e278f7596ffe3f3f68c57c390d025c
woo00oo <96woo94@naver.com>
2022-05-03 23:05:19 +0900

    이펙티브 자바 완벽 공략 : 아이템 5. 완벽 공략 14 - 팩터리 메서드 패턴

--- src/main/java/com/example/effectivejava/chapter01/item05/MockDictionary.java
+++ src/main/java/com/example/effectivejava/chapter01/item05/MockDictionary.java
@@ -1,4 +1,15 @@
 package com.example.effectivejava.chapter01.item05;
 
-public class MockDictionary {
+import java.util.List;
+
+public class MockDictionary implements Dictionary {
+    @Override
+    public boolean contains(String word) {
+        return false;
+    }
+
+    @Override
+    public List<String> closeWordsTo(String typo) {
+        return null;
+    }
 }
