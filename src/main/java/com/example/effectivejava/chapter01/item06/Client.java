079f5fdb7a5bddafc364946d6e3012ee71c3892e
woo00oo <96woo94@naver.com>
2022-05-06 21:31:23 +0900

    이펙티브 자바 완벽 공략 : 아이템 6. 완벽 공략 16 - deprecation

---
+++ src/main/java/com/example/effectivejava/chapter01/item06/Client.java
@@ -0,0 +1,8 @@
+package com.example.effectivejava.chapter01.item06;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Deprecation deprecation = new Deprecation();
+    }
+}
---
+++ src/main/java/com/example/effectivejava/chapter01/item06/Deprecation.java
@@ -0,0 +1,27 @@
+package com.example.effectivejava.chapter01.item06;
+
+public class Deprecation {
+
+    /**
+     * @deprecated in favor of
+     * {@link #Deprecation(String)}
+     */
+    //애노테이션 프로세서
+    @Deprecated(forRemoval = true, since = "1.2")
+    public Deprecation() {
+    }
+
+    private String name;
+
+    public Deprecation(String name) {
+        this.name = name;
+    }
+}
+
+/*
+    @Deprecated
+    - 컴파일시 경고 메시지를 통해 사용 자제를 권장하는 API라는 것을 클라이언트에 알려줄 수 있다.
+
+    @deprecated
+    - 문서화(Javadoc)에 사용해, 왜 해당 API 사용을 지양하며, 그 대신 권장하는 API가 어떤 것인지 표기할 수 있다.
+ */
