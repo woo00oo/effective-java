7c5e0be626b45e00ec3af8bdc775bea5debfe31c
woo00oo <96woo94@naver.com>
2022-05-15 12:09:00 +0900

    이펙티브 자바 완벽 공략 : 아이템 8. 완벽 공략 23 - AutoClosable

--- src/main/java/com/example/effectivejava/chapter01/item08/autoclosable/App.java
+++ src/main/java/com/example/effectivejava/chapter01/item08/autoclosable/App.java
@@ -1,10 +1,12 @@
 package com.example.effectivejava.chapter01.item08.autoclosable;
 
+import java.io.IOException;
+
 public class App {
 
     public static void main(String[] args) {
 
-        try(AutoClosableIsGood good = new AutoClosableIsGood()) {
+        try(AutoClosableIsGood good = new AutoClosableIsGood("")) {
             // 자원 반납 처리가 됨.
         }
     }
--- src/main/java/com/example/effectivejava/chapter01/item08/autoclosable/AutoClosableIsGood.java
+++ src/main/java/com/example/effectivejava/chapter01/item08/autoclosable/AutoClosableIsGood.java
@@ -2,15 +2,25 @@ package com.example.effectivejava.chapter01.item08.autoclosable;
 
 import java.io.*;
 
-public class AutoClosableIsGood implements AutoCloseable{
+public class AutoClosableIsGood implements Closeable{
 
-    private BufferedInputStream inputStream;
+    private BufferedReader reader;
 
+    public AutoClosableIsGood(String path) {
+        try {
+            this.reader = new BufferedReader(new FileReader(path));
+        } catch (FileNotFoundException e) {
+            throw new IllegalArgumentException(path);
+        }
+    }
+
+    /**
+     * close 메소드는 멱등성을 유지 시켜야 함.
+     */
     @Override
     public void close() {
         try {
-            System.out.println("안녕~");
-            inputStream.close();
+            reader.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
