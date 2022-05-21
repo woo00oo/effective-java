875398b268d4d20f58117cc6cf90fe9efe747031
woo00oo <96woo94@naver.com>
2022-05-21 15:34:00 +0900

    이펙티브 자바 완벽 공략 : 아이템 10. 핵심 정리 1, 2, 3

---
+++ src/main/java/com/example/effectivejava/chapter02/item10/Color.java
@@ -0,0 +1,5 @@
+package com.example.effectivejava.chapter02.item10;
+
+public enum Color {
+    RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, VIOLET
+}
---
+++ src/main/java/com/example/effectivejava/chapter02/item10/EqualsInjava.java
@@ -0,0 +1,23 @@
+package com.example.effectivejava.chapter02.item10;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.sql.Timestamp;
+import java.util.Date;
+
+public class EqualsInjava extends Object {
+    public static void main(String[] args) throws MalformedURLException {
+        long time = System.currentTimeMillis();
+        Timestamp timestamp = new Timestamp(time);
+        Date date = new Date(time);
+
+        // 대칭성 위배! P60
+        System.out.println(date.equals(timestamp));
+        System.out.println(timestamp.equals(date));
+
+        // 일관성 위배 가능성 있음. P61
+        URL google1 = new URL("https", "about.google", "/products/");
+        URL google2 = new URL("https", "about.google", "/products/");
+        System.out.println(google1.equals(google2));
+    }
+}
