68a212055e3028f47aa7894cfec835040667237f
woo00oo <96woo94@naver.com>
2022-06-09 23:07:46 +0900

    이펙티브 자바 완벽 공략 : 아이템 14. 핵심 정리 2 - Comparable 구현 방법1

---
+++ src/main/java/com/example/effectivejava/chapter02/item14/inheritance/Point.java
@@ -0,0 +1,28 @@
+package com.example.effectivejava.chapter02.item14.inheritance;
+
+public class Point implements Comparable<Point> {
+
+    int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public int compareTo(Point point) {
+        int result = Integer.compare(this.x, point.x);
+        if (result == 0) {
+            result = Integer.compare(this.y, point.y);
+        }
+        return result;
+    }
+}
